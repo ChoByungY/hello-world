9bdde66c5448aa64ac8f3fc64aca6dd4bdb5c292
ChoByungY <bycho@naver.com>
2023-08-14 21:18:42 +0900

    simpifyed by bycho to make read testing GitHub site

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # hello-world
 test GitHub for bycho
-
-python hello.py added
+python hello.py added in 2023/08/14
--- python/hello.py
+++ python/hello.py
@@ -1,5 +1,5 @@
 # First line
-print("hello world")
+print("hello world and GitHub studying")
 
 # test line
 print("Others program is not coded yet")
