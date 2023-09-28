344e2f54a2de5d9581b8ed0cd714fb20e68bbea8
ByungYoung <bycho@naver.com>
2023-09-29 08:33:42 +0900

    arrays directory added

---
+++ python/arrays/arrays.py
@@ -0,0 +1,22 @@
+data = [1.6, 3.4, 5.5, 9.4]
+N = len(data)
+print(N)
+print(data[2])
+data[2] = 7.3
+print(data[2])
+for x in data :
+    print(x)
+    
+
+data.append(11.4)
+N = len(data)
+print(N)
+for x in data :
+    print(x)
+
+carlist = ["Volvo", "Tesla", "Ford"]
+
+for x in carlist :
+    print(x)
+
+print(carlist[2])
