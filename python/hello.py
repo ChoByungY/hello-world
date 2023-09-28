4fe37f7c3ae9a463815431cfe114fbeb789c9b20
ByungYoung <bycho@naver.com>
2023-09-28 21:44:16 +0900

    complex variable test

--- python/hello.py
+++ python/hello.py
@@ -6,6 +6,14 @@ print("Testing program files are added in each python directory on its own name"
 
 #Sample tets file are like this
 print("Variable test")
+
+x = 1
+y = 2.8
+z = 3 + 2j
+
+print(x,y,z)
+print(type(x), type(y), type(z))
+
 MyVariable = 100 + 300
 print(MyVariable - 400)
 
