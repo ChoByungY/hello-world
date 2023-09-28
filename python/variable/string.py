06965d3e2d1e16f0796b93639bad4fbb9a004d3a
ByungYoung <bycho@naver.com>
2023-09-28 21:57:18 +0900

    variable directory added

---
+++ python/variable/string.py
@@ -0,0 +1,14 @@
+ss = "Hello world"
+
+print(ss)
+print(ss[1])
+print(ss[2:7])
+print(len(ss))
+print(ss.lower())
+print(ss.upper())
+print(ss.replace('H','J'))
+print(ss.split(" "))
+
+print("Enter your name")
+name = input()
+print("Hello, " + name)
