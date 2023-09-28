dbbe8a2eb5cd1dcb81f1caef89e6b9033b04894b
ByungYoung <bycho@naver.com>
2023-09-28 20:51:04 +0900

    Minor change

--- python/random/randomint.py
+++ python/random/randomint.py
@@ -1,7 +1,8 @@
-# This is same with next paragraph
 from random import randint
 
 items = ['a', 'b', 'c', 'd', 'e']
+
+# This is same with next paragraph
 end = len(items) -1
 r = randint(0, end)
 print(items[r])
