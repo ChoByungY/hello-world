14583dfb5840aa581c5517094e1ccf6bacc23677
ByungYoung <bycho@naver.com>
2023-10-06 15:30:00 +0900

    csv_read added

--- python/control_flow/function2.py
+++ python/control_flow/function2.py
@@ -34,3 +34,80 @@ def f(a, L=None):
         L = []
     L.append(a)
     return L
+
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
+
+parrot(1000) # 1 positional argument
+parrot(voltage=1000) # 1 keyword argument
+parrot(voltage=1000000, action='VOOOOOM') # 2 keyword arguments
+parrot(action='VOOOOOM', voltage=1000000) # 2 keyword arguments
+parrot('a million', 'bereft of life', 'jump') # 3 positional arguments
+parrot('a thousand', state='pushing up the daisies') # 1 positional, 1 keyword
+
+
+def function(a):
+    pass
+
+# function(0, a=0)
+
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+
+cheeseshop("Limburger", "It's very runny, sir.",
+        "It's really very, VERY runny, sir.",
+        shopkeeper="Michael Palin",
+        client="John Cleese",
+        sketch="Cheese Shop Sketch")
+
+def write_multiple_items(file, separator, *args):
+    file.write(separator.join(args))
+
+def concat(*args, sep="/"):
+    return sep.join(args)
+
+print(concat("earth", "mars", "venus"))
+print(concat("earth", "mars", "venus", sep="."))
+
+list(range(3, 6)) # normal call with separate arguments
+args = [3, 6]
+list(range(*args))
+
+
+def make_incrementor(n):
+    return lambda x: x + n
+
+f = make_incrementor(42)
+f(0)
+
+f(1)
+
+pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
+pairs.sort(key=lambda pair: pair[1])
+print(pairs)
+
+def my_function():
+    """Do nothing, but document it.
+
+    No, really, it doesn't do anything.
+    """
+    pass
+
+print(my_function.__doc__)
+
+def f(ham: str, eggs: str = 'eggs') -> str:
+    print("Annotations:", f.__annotations__)
+    print("Arguments:", ham, eggs)
+    return ham + ' and ' + eggs
+
+f('spam')
+print(f('spam'))
---
+++ python/csv_read/portfolio.py
@@ -0,0 +1,20 @@
+filename = "/home/bycho/Programs/git-hub/hello-world/python/csv_read/portfolio.csv"
+portfolio = []
+
+for line in open(filename):
+    fields = line.split(",")
+    if fields[0] == 'name' or fields[0][0] == '#':
+        continue
+    name = fields[0]
+    shares = int(fields[1])
+    price = float(fields[2])
+    stock = (name, shares, price)
+    portfolio.append(stock)
+
+
+total = 0.0
+for name, shares, price in portfolio:
+    total = shares * price
+
+print (portfolio)
+print (f" Total price is {total}")
