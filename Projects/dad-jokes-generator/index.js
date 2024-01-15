e6cb69d5169aa0d018a8322df693d18806b90231
ByungYoung <bycho@naver.com>
2024-01-15 18:09:05 +0900

    bug corrected

--- Projects/dad-jokes-generator/index.js
+++ Projects/dad-jokes-generator/index.js
@@ -1,7 +1,9 @@
 const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
 
 
 function getJoke() {
+    jokeEl.innerText = "clicked !! & get the joke from api";
     console.log("clicked");
 }
 
